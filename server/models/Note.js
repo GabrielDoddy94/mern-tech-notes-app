0f93d6f5770c9bd7e445a3081c5e29ee173305f6
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-05 15:40:58 -0300

    mongodb: create a note data model

--- server/models/Note.js
+++ server/models/Note.js
@@ -1,24 +1,33 @@
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
-const noteSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  roles: [
-    {
-      type: String,
-      default: "Employee",
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
-  ],
-  active: {
-    type: Boolean,
-    default: true,
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
+  { timestamps: true }
+);
+
+noteSchema.plugin(AutoIncrement, {
+  inc_field: "ticket",
+  id: "ticketNums",
+  start_seq: 500,
 });
 
 module.exports = mongoose.model("Note", noteSchema);
