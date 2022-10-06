68c2870f663b1e9d1cc91150b6689de14cf80b05
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-06 17:59:53 -0300

    dash routes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,14 +2,20 @@ import { Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Public from "./components/Public";
-import Login from "./components/Login";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+        </Route>
       </Route>
     </Routes>
   );
