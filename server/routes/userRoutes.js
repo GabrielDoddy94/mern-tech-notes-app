c09e67992d633decc313ef3892bc991dc116250c
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-06 15:35:53 -0300

    add controller methods to the user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const usersController = require("../controllers/usersController");
 
-router.route("/").get().post().patch().delete();
+router
+  .route("/")
+  .get(usersController.getAllUsers)
+  .post(usersController.createNewUser)
+  .patch(usersController.updateUser)
+  .delete(usersController.deleteUser);
 
 module.exports = router;
