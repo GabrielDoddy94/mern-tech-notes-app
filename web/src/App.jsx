381d931a707dfcf68f8a552b18cb588fa31c0026
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-06 18:06:48 -0300

    notes and users routes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,6 +5,8 @@ import Public from "./components/Public";
 import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
+import UsersList from "./features/users/UsersList";
+import NotesList from "./features/notes/NotesList";
 
 function App() {
   return (
@@ -15,6 +17,14 @@ function App() {
 
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
+
+          <Route path="users">
+            <Route index element={<UsersList />} />
+          </Route>
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+          </Route>
         </Route>
       </Route>
     </Routes>
