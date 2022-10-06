974563a4c2a4f408113172f7d9e10246781bca40
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-06 17:17:45 -0300

    apply basic routing

---
+++ web/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
