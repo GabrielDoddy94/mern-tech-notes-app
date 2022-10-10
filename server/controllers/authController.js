78d9f012b79073602381df29da7473e1425b57ee
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-10 18:06:03 -0300

    refresh method in auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -56,7 +56,36 @@ const login = asyncHandler(async (req, res) => {
 // @route GET /auth/refresh
 // @access Public - because access token has expired
 const refresh = (req, res) => {
-  // do stuff
+  const cookies = req.cookies;
+
+  if (!cookies?.jwt) return res.status(401).json({ message: "Unauthorized" });
+
+  const refreshToken = cookies.jwt;
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    asyncHandler(async (err, decoded) => {
+      if (err) return res.status(403).json({ message: "Forbidden" });
+
+      const foundUser = await User.findOne({ username: decoded.username });
+
+      if (!foundUser) return res.status(401).json({ message: "Unauthorized" });
+
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            username: foundUser.username,
+            roles: foundUser.roles,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "10S" }
+      );
+
+      res.json({ accessToken });
+    })
+  );
 };
 
 // @desc Logout
