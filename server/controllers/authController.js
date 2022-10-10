bf38d408d7f978d6283f3b8f72769fb9d15e1c2a
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-10 17:55:39 -0300

    login method in auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,7 +7,49 @@ const asyncHandler = require("express-async-handler");
 // @route POST /auth
 // @access Public
 const login = asyncHandler(async (req, res) => {
-  // do stuff
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const foundUser = await User.findOne({ username }).exec();
+
+  if (!foundUser || !foundUser.active) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const match = await bcrypt.compare(password, foundUser.password);
+
+  if (!match) return res.status(401).json({ message: "Unauthorized" });
+
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        username: foundUser.username,
+        roles: foundUser.roles,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "10s" }
+  );
+
+  const refreshToken = jwt.sign(
+    { username: foundUser.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  // Create secure cookie with refresh token
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true, // accessible only by the web server
+    secure: true, // accessible only by web server
+    sameSite: "none", // cross-site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expiry: set to match rT
+  });
+
+  // Send accessToken containing username and roles
+  res.json({ accessToken });
 });
 
 // @desc Refresh
