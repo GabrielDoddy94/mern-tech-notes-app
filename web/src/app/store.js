94dfe09dfd34f79a637e98749cf2cca655b8bf01
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-12 16:05:55 -0300

    redux: authSlice

--- web/src/app/store.js
+++ web/src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 import { apiSlice } from "./api/apiSlice";
+import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(apiSlice.middleware),
