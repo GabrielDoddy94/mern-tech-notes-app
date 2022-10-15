9d0bccec20997e192984df5a72eb7b3da044f65c
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-15 18:19:36 -0300

    code refactor: EditUser component refactor

--- web/src/features/users/EditUser.jsx
+++ web/src/features/users/EditUser.jsx
@@ -1,16 +1,22 @@
-import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import PulseLoader from "react-spinners/PulseLoader";
 
-import { selectUserById } from "./usersApiSlice";
+import { useGetUsersQuery } from "./usersApiSlice";
 
 import EditUserForm from "./EditUserForm";
 
 const EditUser = () => {
   const { id } = useParams();
 
-  const user = useSelector(state => selectUserById(state, id));
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => {
+      user: data?.entities[id];
+    },
+  });
 
-  const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>;
+  if (!user) return <PulseLoader color={"#fff"} />;
+
+  const content = <EditUserForm user={user} />;
 
   return content;
 };
