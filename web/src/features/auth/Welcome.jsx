a30744e531924463a136099754cd981fb65c0932
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-15 14:48:18 -0300

    welcome username & links

--- web/src/features/auth/Welcome.jsx
+++ web/src/features/auth/Welcome.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 
+import useAuth from "../../hooks/useAuth";
+
 const Welcome = () => {
+  const { username, isManager, isAdmin } = useAuth();
+
   const date = new Date();
   const today = new Intl.DateTimeFormat("en-US", {
     dateStyle: "full",
@@ -10,19 +14,23 @@ const Welcome = () => {
   const content = (
     <section className="welcome">
       <p>{today}</p>
-      <h1>Welcome!</h1>
+      <h1>Welcome {username}!</h1>
       <p>
         <Link to="/dash/notes">View techNotes</Link>
       </p>
       <p>
         <Link to="/dash/notes/new">Add New techNote</Link>
       </p>
-      <p>
-        <Link to="/dash/users">View User Settings</Link>
-      </p>
-      <p>
-        <Link to="/dash/users/new">Add New User</Link>
-      </p>
+      {(isAdmin || isManager) && (
+        <p>
+          <Link to="/dash/users">View User Settings</Link>
+        </p>
+      )}
+      {(isAdmin || isManager) && (
+        <p>
+          <Link to="/dash/users/new">Add New User</Link>
+        </p>
+      )}
     </section>
   );
 
