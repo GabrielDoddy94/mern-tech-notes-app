951228edd3d741ce92fc42d9a0007b843dbb7787
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-15 10:26:28 -0300

    persist login component

--- web/src/features/auth/Login.jsx
+++ web/src/features/auth/Login.jsx
@@ -4,6 +4,7 @@ import { useNavigate, Link } from "react-router-dom";
 
 import { setCredentials } from "./authSlice";
 import { useLoginMutation } from "./authApiSlice";
+import usePersist from "../../hooks/usePersist";
 
 const Login = () => {
   const userRef = useRef();
@@ -11,6 +12,7 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errMsg, setErrMsg] = useState("");
+  const [persist, setPersist] = usePersist();
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -51,6 +53,7 @@ const Login = () => {
 
   const handleUserInput = e => setUsername(e.target.value);
   const handlePwdInput = e => setPassword(e.target.value);
+  const handleToggle = () => setPersist(prev => !prev);
 
   const errClass = errMsg ? "errmsg" : "offscreen";
 
@@ -90,6 +93,17 @@ const Login = () => {
             required
           />
           <button className="form__submit-button">Sign In</button>
+
+          <label className="form__persist" htmlFor="persist">
+            <input
+              type="checkbox"
+              id="persist"
+              className="form__checkbox"
+              onChange={handleToggle}
+              checked={persist}
+            />
+            Trust This Device
+          </label>
         </form>
       </main>
 
