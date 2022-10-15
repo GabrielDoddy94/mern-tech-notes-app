dec41694628b96196f974d18bca5d2c6ac4c4a0d
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-15 17:44:03 -0300

    code refactor: additional error flag in handler

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -10,7 +10,7 @@ const errorHandler = (err, req, res, next) => {
   const status = res.statusCode ? res.statusCode : 500; // server error
   res.status(status);
 
-  res.json({ message: err.message });
+  res.json({ message: err.message, isError: true });
 };
 
 module.exports = errorHandler;
