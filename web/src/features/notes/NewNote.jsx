a2228bbe3a66ab8f1be820c6e766e2b0daef9858
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-15 18:25:53 -0300

    code refactor: NewNote component refactor

--- web/src/features/notes/NewNote.jsx
+++ web/src/features/notes/NewNote.jsx
@@ -1,13 +1,17 @@
-import { useSelector } from "react-redux";
+import PulseLoader from "react-spinners/PulseLoader";
 
-import { selectAllUsers } from "../users/usersApiSlice";
+import { useGetUsersQuery } from "../users/usersApiSlice";
 
 import NewNoteForm from "./NewNoteForm";
 
 const NewNote = () => {
-  const users = useSelector(selectAllUsers);
+  const { users } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => {
+      users: data?.ids.map(id => data?.entities[id]);
+    },
+  });
 
-  if (!users?.length) return <p>Not Currently Available</p>;
+  if (!users?.length) return <PulseLoader color={"#fff"} />;
 
   const content = <NewNoteForm users={users} />;
 
