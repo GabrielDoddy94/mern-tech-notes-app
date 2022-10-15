e80d77ebabdb4571b9cc7f18791762d39e99a444
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-15 15:43:20 -0300

    notes list access

--- web/src/features/notes/NotesList.jsx
+++ web/src/features/notes/NotesList.jsx
@@ -1,8 +1,11 @@
 import { useGetNotesQuery } from "./notesApiSlice";
+import useAuth from "../../hooks/useAuth";
 
 import Note from "./Note";
 
 const NotesList = () => {
+  const { username, isManager, isAdmin } = useAuth();
+
   const {
     data: notes,
     isLoading,
@@ -24,11 +27,20 @@ const NotesList = () => {
   }
 
   if (isSuccess) {
-    const { ids } = notes;
+    const { ids, entities } = notes;
 
-    const tableContent = ids?.length
-      ? ids.map(noteId => <Note key={noteId} noteId={noteId} />)
-      : null;
+    let filteredIds;
+    if (isManager || isAdmin) {
+      filteredIds = [...ids];
+    } else {
+      filteredIds = ids.filter(
+        noteId => entities[noteId].username === username
+      );
+    }
+
+    const tableContent =
+      ids?.length &&
+      filteredIds.map(noteId => <Note key={noteId} noteId={noteId} />);
 
     content = (
       <table className="table table--notes">
