6c1c5f4ae47badf68fca19982c7bb0fb11c11edb
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-16 13:35:11 -0300

    update url

--- web/src/app/api/apiSlice.js
+++ web/src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-api.onrender.com",
+  baseUrl: "https://technotes-api-jsor.onrender.com/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
