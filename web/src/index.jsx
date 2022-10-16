7e8cb5e3487f17215b5f42de002446bf68c8c85a
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-16 11:43:28 -0300

    frontend: ready to deploy

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 import { store } from "./app/store";
 
 import "./index.css";
 import App from "./App";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
